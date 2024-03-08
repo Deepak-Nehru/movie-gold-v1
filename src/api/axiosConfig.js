d95ddb0be8a68059c9c934dc1ea447998dfc2a47
Deepak N <deepaknehru1999@gmail.com>
2024-03-08 15:21:12 +0530

    new

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-baseURL: 'http://moviesbackend-env.eba-k8ymxgmc.ap-south-1.elasticbeanstalk.com',
+baseURL: 'https://d2reo2yoy0hqdo.cloudfront.net',
 headers: {"skip-browser-warning": "true"}
 }); 
